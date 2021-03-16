b98d395c55f6898dde4884ba767b2f4cea5946a1
Anbrela <pablonaveiralopez@gmail.com>
2021-03-16 11:34:10 +0100

    --initial commit

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+
+
+const socketio = require("socket.io");
+const io = socketio(server);
+
+io.on("connection", socket => {
+
+    socket.on('conectado', (nombre) => {
+        console.log("usuario conectado", nombre);
+    })
+
+    socket.on("mensaje", (nombre, message) => {
+        io.emit("messages", {nombre, message});
+    })
+
+    socket.on("disconnect", () => {
+        io.emit("messages", {nombre:"server", nombre: "Ha abandonado la sala" });
+    })
+})
+
+server.listen(4000, () => console.log("server started on port 4000"))
