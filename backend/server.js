2db91ccbf222a56c567b92f086f2fecd8087ba39
Anbrela <pablonaveiralopez@gmail.com>
2021-03-23 17:30:31 +0100

    -- CORS

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,27 @@
 const express = require("express");
 const http = require("http");
+const cors = require("cors");
 const app = express();
 const server = http.createServer(app);
+app.use(cors());
+const io = require("socket.io")(server, {
+  cors: {
+    origins: "*:*",
+  },
+});
 
+io.on("connection", (socket) => {
+  socket.on("conectado", (nombre) => {
+    console.log("usuario conectado", nombre);
+  });
 
+  socket.on("mensaje", (nombre, message) => {
+    io.emit("messages", { nombre, message });
+  });
 
+  socket.on("disconnect", () => {
+    io.emit("messages", { nombre: "server", nombre: "Ha abandonado la sala" });
+  });
+});
 
-const io = require('socket.io')(server, { origins: '*:*'});
-
-
-io.on("connection", socket => {
-
-    socket.on('conectado', (nombre) => {
-        console.log("usuario conectado", nombre);
-    })
-
-    socket.on("mensaje", (nombre, message) => {
-        io.emit("messages", {nombre, message});
-    })
-
-    socket.on("disconnect", () => {
-        io.emit("messages", {nombre:"server", nombre: "Ha abandonado la sala" });
-    })
-})
-
-server.listen(4000, () => console.log("server started on port 4000"))
+server.listen(4000, () => console.log("server started on port 4000"));
--- front/src/components/Socket/Socket.js
+++ front/src/components/Socket/Socket.js
@@ -1,10 +1,5 @@
 import io from "socket.io-client";
 
-const socket = io("http://localhost:4000", {
-  withCredentials: true,
-  extraHeaders: {
-    "my-custom-header": "abcd"
-  }
-});
+let socket = io("//localhost:4000");
 
 export default socket;
